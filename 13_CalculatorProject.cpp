595f65172b3268bc46e8199b54da5b0b6aee5d43
deepaknirala <deepakniralabgp@gmail.com>
2025-05-10 12:09:12 +0530

    Calculator

--- 13_CalculatorProject.cpp
+++ 13_CalculatorProject.cpp
@@ -12,38 +12,38 @@ int main(){
   while(1){
     cin>>num1>>operation>>num2;
     cin.ignore(100,'\n'); //to ignore any other inputs after the second value
-    if(cin.fail()){
+    if(cin.fail()){     //if user enters letters mixed with numbers or any such invalid inputs
       cout<<"Invalid input!"<<'\n'<<"...exiting!"<<endl;
       return 0;}
     cin.clear(); //cleaning the input buffer to avoid issue with next input
     
     switch(operation){
-        case '+':
+        case '+':   //Addition
         result = num1 + num2;
         cout<<num1<<operation<<num2<<" = "<<result<<endl;
         break;
-        case '-':
+        case '-':   //Subtraction
         result = num1 - num2;
         cout<<num1<<operation<<num2<<" = "<<result<<endl;
         break;
-        case '*':
+        case '*':   //Multiplication
         result = num1 * num2;
         cout<<num1<<operation<<num2<<" = "<<result<<endl;
         break;
-        case '/':
+        case '/':   //Division
         result = num1 / num2;
         cout<<num1<<operation<<num2<<" = "<<result<<endl;
         break;
-        case '%':
+        case '%':   //Modulus
         result = int(num1) % int(num2);
         cout<<num1<<operation<<num2<<" = "<<result<<endl;
         break;
-        case '^':
+        case '^':   //Power
         result = pow(num1, num2);
         cout<<num1<<" to the power "<<num2<<" = "<<result<<endl;
         break;
 
-        case 'r': //rth root of num2
+        case 'r':   //rth root of num2
           if(num2==0){
             cout<<"Undefined!"<<endl;
             break;}
@@ -56,7 +56,7 @@ int main(){
             cout<<num2<<"th root of "<<num1<<" = "<<result<<endl;}
         break;
 
-        default:
+        default:    //Any other invalid operation
         cout<<"Invalid operation!"<<endl;
         break;
       }
