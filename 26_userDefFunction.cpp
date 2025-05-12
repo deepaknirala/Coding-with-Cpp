34afd80a26eb4bba3624cc3b6e3a0e9dd4b0c5fc
deepaknirala <deepakniralabgp@gmail.com>
2025-05-12 12:52:45 +0530

    user Defined function

---
+++ 26_userDefFunction.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+  using namespace std;
+
+//function is a block of reusable code that performs a specific task
+// we have to declare function before main() if we call it in main
+//& we may define it after main
+
+//OR simply we can declare n define it before main
+//-------------------------------------------------------
+void happyBirthday(string name, int age); //funtion declaration
+
+int main(){
+  string name;
+  int age;
+  cout<<"Enter your name: ";
+  cin>>name;
+  cin.ignore(25,'\n');//if user give full name, it will only take first name till space
+  
+  cout<<"Enter your age: ";
+  cin>>age;
+  happyBirthday(name,age);  //function call
+
+  return 0;
+}
+
+//function definition
+void happyBirthday(string name, int age){
+  cout<<"Happy Birthday "<<name<<'\n';
+  cout<<"Happy Birthday to you dear "<<name<<"!"<<'\n';
+  cout<<"You are "<<age<<" years old now!\n";
+}
