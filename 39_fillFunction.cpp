51d35b07a797ef255ab789bbe7c64bc7d0c7769e
deepaknirala <deepakniralabgp@gmail.com>
2025-05-16 21:54:23 +0530

    fill function added comment

--- 39_fillFunction.cpp
+++ 39_fillFunction.cpp
@@ -3,13 +3,14 @@
 //fill() func fills a range of elements with a specified value
 //fill(begin, end, value) 
 int main() {
-  const int SIZE=100;
+  const int SIZE=15;
   std::string foods[SIZE];
   fill(foods, foods+(SIZE/2), "pizza");
   //parameters are pointers to the first and one past the last element of the array
   fill(foods+(SIZE/2), foods+SIZE, "burger");
-  for(std::string food : foods) {
-    std::cout << food << " ";
+
+  for(std::string food : foods) { //printing contents of the array using range-based for loop
+    std::cout<<food<<"\n";
   }
   return 0;
 }
