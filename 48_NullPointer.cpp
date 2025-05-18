3d2991d850325ec522fe16c15d91aaafb702e3d1
deepaknirala <deepakniralabgp@gmail.com>
2025-05-18 21:54:59 +0530

    nullptr keyword

---
+++ 48_NullPointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+//Null pointer is a pointer that does not point to any object or function.
+// It is a pointer that has not been assigned a valid memory address.
+//when a ptr is holding a null value it is not pointing to anything.
+
+// nullptr is a keyword in C++ to represent a null pointer literal
+
+int main() {
+  int *pointer = nullptr; // Initialize a pointer to null
+  int x=12345;
+  pointer = &x; // assign a valid address to the pointer
+
+  if(pointer==nullptr){
+    std::cout<<"address was not assigned!\n";
+    //std::cout<<*pointer<<std::endl; // Dereferencing a null pointer leads to undefined behavior
+  }
+  else{
+    std::cout<<"address was assigned!\n";
+    std::cout<<*pointer<<std::endl;
+  }
+  return 0;
+}
